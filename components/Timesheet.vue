2514667f13544ad24d558ea72f7da77b30edd4e3
Paul Bastowski <pbastowski@gmail.com>
2019-10-25 16:59:58 +0200

    inputs for all timesheet entries

--- components/Timesheet.vue
+++ components/Timesheet.vue
@@ -6,9 +6,32 @@
             Xitems-per-page="15"
             class="elevation-1"
         >
+            <template v-slot:item.date="{ item }">
+                <v-text-field v-model="item.date" />
+            </template>
+            <template v-slot:item.worked="{ item }">
+                <v-text-field v-model="item.worked" />
+            </template>
+            <template v-slot:item.internal="{ item }">
+                <v-text-field v-model="item.internal" />
+            </template>
+            <template v-slot:item.courses="{ item }">
+                <v-text-field v-model="item.courses" />
+            </template>
+            <template v-slot:item.leave="{ item }">
+                <v-text-field v-model="item.leave" />
+            </template>
+            <template v-slot:item.sick="{ item }">
+                <v-text-field v-model="item.sick" />
+            </template>
+            <template v-slot:item.doctor="{ item }">
+                <v-text-field v-model="item.doctor" />
+            </template>
         </v-data-table>
     </div>
 </template>
+
+<!--                    {{ item[headers[index] && headers[index].value] }}-->
 
 <script>
 export default {
