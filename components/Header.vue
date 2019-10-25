1e107addc6a006f984a299570b9bfdf278faa71f
Da Man <lfrecia@debian.fibertel.com.ar>
2019-10-25 15:50:09 +0200

    added user links to header

--- components/Header.vue
+++ components/Header.vue
@@ -1,27 +1,66 @@
 <template>
-    <div>
-        <h4 style="position: absolute; right: 20px; top: 10px;">
-            <div v-if="$auth.loggedIn">
-                User: {{ $auth.user.username }}
-                <br />
-                <button @click="$auth.logout()">logout</button>
-            </div>
-            <div v-else>
-                <nuxt-link to="/login">login</nuxt-link>
-            </div>
-        </h4>
-
-        <nuxt-link to="/">home</nuxt-link>
-        <nuxt-link to="public">public</nuxt-link>
-        <nuxt-link to="secret">secret</nuxt-link>
-        <nuxt-link to="profile">profile</nuxt-link>
-        <nuxt-link to="timesheet/timesheet-new">New Timesheet</nuxt-link>
+    <div class="d-flex align-center" v-if="$auth.loggedIn">
+        <div class="v-toolbar__title">User: {{ $auth.user.username }}</div>
+        <v-menu left bottom>
+            <template v-slot:activator="{ on }">
+                <v-btn icon v-on="on">
+                    <v-icon>mdi-dots-vertical</v-icon>
+                </v-btn>
+            </template>
+            <v-list>
+                <v-list-item>
+                    <v-list-item-title @click="() => {}">
+                        <nuxt-link class="no-link-style" to="/">Home</nuxt-link>
+                    </v-list-item-title>
+                </v-list-item>
+                <v-list-item>
+                    <v-list-item-title @click="() => {}">
+                        <nuxt-link class="no-link-style" to="public">Public</nuxt-link>
+                    </v-list-item-title>
+                </v-list-item>
+                <v-list-item>
+                    <v-list-item-title @click="() => {}">
+                        <nuxt-link class="no-link-style" to="secret">Secret</nuxt-link>
+                    </v-list-item-title>
+                </v-list-item>
+                <v-list-item>
+                    <v-list-item-title @click="() => {}">
+                        <nuxt-link class="no-link-style" to="profile">Profile</nuxt-link>
+                    </v-list-item-title>
+                </v-list-item>
+                <v-list-item>
+                    <v-list-item-title @click="() => {}">
+                        <nuxt-link class="no-link-style" to="timesheet/timesheet-new">New Timesheet</nuxt-link>
+                    </v-list-item-title>
+                </v-list-item>
+                <v-list-item>
+                    <v-list-item-title>
+                        <button @click="$auth.logout()">Logout</button>
+                    </v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
     </div>
+    <div v-else></div>
 </template>
-
+                        
 <script>
 export default {}
 </script>
+
+<style>
+.no-link-style {
+    text-decoration: inherit;
+    color: inherit;
+    cursor: auto;
+}
+
+.no-link-style:visited {
+    text-decoration: inherit;
+    color: inherit;
+    cursor: auto;
+}
+</style>
 
 <style>
 </style>
