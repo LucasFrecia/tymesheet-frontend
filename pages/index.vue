3538b331abd4a2b8db00a2243b69900a2e3deb52
Paul Bastowski <pbastowski@gmail.com>
2019-10-25 15:34:52 +0200

    begin timesheet

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,16 @@
 <template>
     <div>
-        <h1> Welcome! </h1>
+        <h1>Welcome!</h1>
+        <Timesheet />
     </div>
 </template>
 
 <script>
+import Timesheet from '~/components/Timesheet'
 
+export default {
+    components: {
+        Timesheet
+    }
+}
 </script>
