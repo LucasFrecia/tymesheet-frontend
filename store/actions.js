3e5fe1c5318231ab75fa4014b6c2e985cd853998
Paul Bastowski <pbastowski@gmail.com>
2019-10-25 18:00:05 +0200

    save the timesheet

--- store/actions.js
+++ store/actions.js
@@ -6,10 +6,9 @@ export async function getTimesheet({ state }, { date, client }) {
     return state.timesheet
 }
 
-export async function saveTimesheet({ state }, { date, client }) {
-    state.timesheet = (await this.$axios.$post(`/api/months`, {
-        search: { date, client }
-    }))[0]
-
-    return state.timesheet
+export async function saveTimesheet({ state }, { date, client } = {}) {
+    return this.$axios.$post(
+        `/api/months/${state.timesheet.id}`,
+        state.timesheet
+    )
 }
