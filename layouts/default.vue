63e0425c462b5d41a6bb71418ffe6567424f9675
Dennis Bruijn <dbbruijn@gmail.com>
2019-10-25 14:56:40 +0200

    feature: add appshell

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,93 @@
 <template>
-    <v-app>
-        <TheHeader />
-        <nuxt />
+    <v-app id="inspire">
+        <v-navigation-drawer v-model="drawer" app>
+            <v-list dense>
+                <v-list-item @click>
+                    <v-list-item-action>
+                        <v-icon>mdi-account</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>Dennis Bruijn</v-list-item-title>
+                        <v-list-item-subtitle>dennis.bruijn@sytac.io</v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item @click>
+                    <v-list-item-action>
+                        <v-icon>mdi-calendar</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>Timesheets</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item @click>
+                    <v-list-item-action>
+                        <v-icon>mdi-history</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>History</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item @click>
+                    <v-list-item-action>
+                        <v-icon>mdi-lifebuoy</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>Help</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-app-bar app color="indigo" dark>
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-toolbar-title>Sytac timesheets</v-toolbar-title>
+        </v-app-bar>
+
+        <v-content>
+            <v-container class="fill-height" fluid>
+                <v-row align="center" justify="center">
+                    <v-col class="text-center">
+                        <v-tooltip left>
+                            <template v-slot:activator="{ on }">
+                                <v-btn :href="source" icon large target="_blank" v-on="on">
+                                    <v-icon large>mdi-code-tags</v-icon>
+                                </v-btn>
+                            </template>
+                            <span>Source</span>
+                        </v-tooltip>
+
+                        <v-tooltip right>
+                            <template v-slot:activator="{ on }">
+                                <v-btn
+                                    icon
+                                    large
+                                    href="https://codepen.io/johnjleider/pen/zgxeLQ"
+                                    target="_blank"
+                                    v-on="on"
+                                >
+                                    <v-icon large>mdi-codepen</v-icon>
+                                </v-btn>
+                            </template>
+                            <span>Codepen</span>
+                        </v-tooltip>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-content>
+        <v-footer color="indigo" app>
+            <span class="white--text">&copy; Sytac timesheets 2019</span>
+        </v-footer>
     </v-app>
 </template>
 
 <script>
-
-import TheHeader from '~/components/Header'
-
 export default {
-     components: {
-        TheHeader
-     }
+    props: {
+        source: String
+    },
+
+    data: () => ({
+        drawer: null
+    })
 }
 </script>
